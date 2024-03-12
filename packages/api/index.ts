6554e21e5b5ba302682d803b2fee48d29826779e
nvos <personal@creoi.dev>
2024-03-12 13:38:55 +0100

    feat: google proto rpc generation

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -3,3 +3,4 @@ export * from './user/v1/user_connect';
 export * from './recipe/v1/recipe_pb';
 export * from './recipe/v1/recipe_connect';
 export * from './client';
+export { BadRequest } from './google/rpc/error_details_pb';
